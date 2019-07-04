f752a41246ba38858aa7b5ade57638804e70aaea
AndriiSotnikov <andrewsotnikov88@gmail.com>
2019-07-04 19:24:01 +0300

    Updates

---
+++ precache-manifest.1b1a9c7ae670c7dd6f7c6471f6064a81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0540b8d330d8342e25c1eca7b66b0e",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "68d5e54ec56f25b08288",
+    "url": "/react_static-list-of-posts/static/css/main.8a7df6d3.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-posts/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "68d5e54ec56f25b08288",
+    "url": "/react_static-list-of-posts/static/js/main.6da2b88a.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
